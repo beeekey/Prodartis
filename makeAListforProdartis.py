f97247b3194bbda36313619e22e6f4038b44c637
ben <bekoch@gmail.com>
2017-08-23 08:49:58 +0200

    Excel has now a title row and some standard-values

--- makeAListforProdartis.py
+++ makeAListforProdartis.py
@@ -25,13 +25,27 @@ sheet.title = 'prodartis_'+str(now.year)+'_'+str(now.month)+'_'+str(now.day)
 wb.get_sheet_names()
 sheet = wb.get_active_sheet()
 
-i=1
+
+### insert the title-row
+i = 1
+#title_row = ['Anzahl', 'Bauteilname', 'Technologie', 'Material', 'Nachbearbeitung', 'Farbe']
+title_row = ['Anzahl/Pieces', 'Teilename/Partname', 'Material', 'Nachbearbeitung/Rework', 'Farbe/Color']
+for j, title_val in enumerate(title_row):
+    sheet[chr(65 + j) + str(i)] = title_val
+
+
+
+### insert the filenames
+i += 2
 for file in files:
     partName=file.split(".")
     #partName=partName.replace("ü","ue")
     partName[0]=unicodedata.normalize('NFKD', u''+partName[0]+'').encode('iso-8859-1', 'ignore')
     print(partName[0])
     sheet['B'+str(i)] = partName[0]
+    sheet['C'+str(i)] = 'PA12 HF'
+    sheet['D'+str(i)] = 'gleitgeschliffen'
+    sheet['E'+str(i)] = 'schwarz/black'
     i+=1
 
 wb.save('prodartis_'+str(now.year)+'_'+str(now.month)+'_'+str(now.day)+'.xlsx')
